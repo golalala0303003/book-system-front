7c8e07f6903004cd0257ed318783859932787bf9
zengrui <2531594414@qq.com>
2026-03-13 17:16:13 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>hello world</div>
+  <el-button type="primary" :icon="Delete" @Click="OnDeleteBtn"/>
+</template>
+
+<script setup lang="ts">
+import { Delete } from '@element-plus/icons-vue'
+
+function OnDeleteBtn() {
+  console.log("someone push delete btn")
+}
+</script>
